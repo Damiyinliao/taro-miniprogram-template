9a189fa8e9f8aa6b2540a6d699142d79238d430f
Damiyinliao <2911493102@qq.com>
2024-08-01 15:29:32 +0800

    feat: 基础配置

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,6 +3,32 @@ export default defineAppConfig({
     'pages/home/index',
     'pages/mine/index',
   ],
+  subPackages: [
+    {
+      root: 'pages_sub/taro-ui',
+      pages: [
+        'picker/index'
+      ]
+    }
+  ],
+  tabBar: {
+    color: '#000',
+    selectedColor: '#0E595F',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页',
+        iconPath: 'assets/images/home.png',
+        selectedIconPath: 'assets/images/home-selected.png'
+      },
+      {
+        pagePath: 'pages/mine/index',
+        text: '我的',
+        iconPath: 'assets/images/smile.png',
+        selectedIconPath: 'assets/images/smile-selected.png'
+      }
+    ]
+  },
   window: {
     navigationStyle: 'custom',
     backgroundTextStyle: 'light',
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
 import { createApp } from 'vue'
-import { useAppStore } from './store/modules'
-import store from './store'
-import component from './utils/component'
+import { setupStore, useAppStore } from './store'
+import { setupComponents } from './components'
 import { log } from './utils'
 import './app.scss'
 
 const App = createApp({
   onShow (options) {
-    log.success('App onShow.')
+    log.success('App onShow.');
   },
   onLaunch (options) {
-    log.success('App onLaunch.')
+    log.success('App onLaunch.');
     const appStore = useAppStore();
     appStore.setDeviceInfo();
   }
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 
-App.use(store)
-component.install(App)  // 注册全局组件
+setupStore(App);
+setupComponents(App);  // 注册全局组件
 export default App
