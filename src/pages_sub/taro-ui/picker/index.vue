5179c076bd041ec4e9ad367b2e8e402da302baa8
Damiyinliao <2911493102@qq.com>
2024-08-01 15:32:08 +0800

    feat: taro-ui picker

---
+++ src/pages_sub/taro-ui/picker/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="taroui-picker-usage-page">
+    <CustomNavbar title="TaroUI-Picker" :loading="false"/>
+  </view>
+</template>
+
+<script setup lang="ts">
+import CustomNavbar from '@/components/navbar/index.vue';
+</script>
+
+<style lang="scss">
+.taroui-picker-usage-page {
+
+}
+</style>
