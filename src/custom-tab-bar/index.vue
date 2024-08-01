815dc61184711f5cf8d287f463e10dd702e5cdb6
Damiyinliao <2911493102@qq.com>
2024-08-01 15:28:33 +0800

    feat: 自定义底部导航栏

---
+++ src/custom-tab-bar/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <nut-config-provider>
+    <nut-tabbar v-model="active" @tab-switch="tabSwitch" bottom safe-area-inset-bottom placeholder>
+      <nut-tabbar-item
+        v-for="(item, index) in List"
+        :key="index"
+        :tab-title="item.title"
+        :icon="item.icon"
+      >
+      </nut-tabbar-item>
+    </nut-tabbar>
+  </nut-config-provider>
+</template>
+
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { Home, Category, Find, Cart, My } from '@nutui/icons-vue-taro'
+
+const active = ref(0);
+
+const List = ref([
+  {
+    title: 'Home',
+    icon: h(Home)
+  },
+  {
+    title: 'Category',
+    icon: h(Category)
+  },
+  {
+    title: 'Find',
+    icon: h(Find)
+  },
+  {
+    title: 'Cart',
+    icon: h(Cart)
+  },
+  {
+    title: 'My',
+    icon: h(My)
+  }
+])
+
+const tabSwitch = (item: Record<string, unknown>, index: number) => {
+  console.log(item, index)
+}
+</script>
+
+<style lang="scss">
+
+</style>
