db87b58921ee43d86d90d3b78c911a83ce73a220
Damiyinliao <2911493102@qq.com>
2024-08-04 21:23:05 +0800

    feat: icon组件

---
+++ src/pages_sub/ui/icon/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <page-wrapper nav-title="Icon">
+    <WeIcon />
+  </page-wrapper>
+</template>
+
+<script setup lang="ts">
+import WeIcon from './components/WeIcon.vue';
+</script>
+
+<style lang="scss">
+
+</style>
