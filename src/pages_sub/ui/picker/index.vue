41d6be0b907638c0fb4eeffc88893ee66e01afcb
Damiyinliao <2911493102@qq.com>
2024-08-04 21:23:23 +0800

    feat: picker组件

---
+++ src/pages_sub/ui/picker/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <page-wrapper nav-title="Picker">
+    <WePicker></WePicker>
+  </page-wrapper>
+</template>
+
+<script setup lang="ts">
+import WePicker from './components/WePicker.vue'
+</script>
+
+<style lang="scss">
+
+</style>
