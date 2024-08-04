fd667d790c4a177b7a4db70213a76a621d4ff479
Damiyinliao <2911493102@qq.com>
2024-08-04 16:18:28 +0800

    feat: components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
-import CustomNavbar from '@/components/navbar/index.vue'
-import SvgIcon from '@/components/svg-icon/index.vue'
+import CustomNavbar from '@/components/taro-navbar/index.vue';
+import SvgIcon from '@/components/svg-icon/index.vue';
+import ExampleItem from '@/components/example-item/index.vue';
+import FontIcon from '@/components/font-icon/index.vue';
 import type { App } from 'vue'
 
 // 注册全局组件
 export function setupComponents(app: App) {
   app.component('TaroNavbar', CustomNavbar);
   app.component('SvgIcon', SvgIcon);
+  app.component('FontIcon', FontIcon);
+  app.component('ExampleItem', ExampleItem)
 }
