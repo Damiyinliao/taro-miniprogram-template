f0e4f37877d7e8f7101e9bbc90b8b1f2c775560d
Damiyinliao <2911493102@qq.com>
2024-08-08 09:45:58 +0800

    feat: common constants

---
+++ src/config/constant.ts
@@ -0,0 +1,18 @@
+export const defaultTabbarConfig = {
+  color: '#000',
+  selectedColor: '#0E595F',
+  list: [
+    {
+      pagePath: 'pages/home/index',
+      text: '首页',
+      iconPath: 'assets/images/home.png',
+      selectedIconPath: 'assets/images/home-selected.png'
+    },
+    {
+      pagePath: 'pages/mine/index',
+      text: '我的',
+      iconPath: 'assets/images/smile.png',
+      selectedIconPath: 'assets/images/smile-selected.png'
+    }
+  ]
+}
