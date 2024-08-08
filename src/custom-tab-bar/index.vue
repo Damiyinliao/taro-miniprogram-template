8baf87f3cf4f2ad0ef9fc5a9b1111f2b23ba622e
Damiyinliao <2911493102@qq.com>
2024-08-08 09:44:24 +0800

    feat: custom tabbar

--- src/custom-tab-bar/index.vue
+++ src/custom-tab-bar/index.vue
@@ -1,51 +1,106 @@
 <template>
-  <nut-config-provider>
-    <nut-tabbar v-model="active" @tab-switch="tabSwitch" bottom safe-area-inset-bottom placeholder>
-      <nut-tabbar-item
-        v-for="(item, index) in List"
-        :key="index"
-        :tab-title="item.title"
-        :icon="item.icon"
-      >
-      </nut-tabbar-item>
-    </nut-tabbar>
-  </nut-config-provider>
+  <view class="tab-bar">
+    <view class="tabbar-list">
+        <view
+          class="tabbar-item"
+          v-for="(item, index) in tabList"
+          :key="item.path"
+          :data-idx="index"
+          :data-path="item.path"
+
+          @tap="handleSwitch"
+        >
+          <view class="tabbar-item__inner" :class="{active: selected === index}">
+            <weapp-svg
+              :name="item.icon"
+              :size="item.iconSize"
+              :color="selected === index ? item.iconColorSelected : item.iconColor"
+            ></weapp-svg>
+            <text class="tabbar-text" :class="{active: selected === index}">{{ item.text }}</text>
+          </view>
+        </view>
+    </view>
+    <view class="zhanwei"></view>
+  </view>
 </template>
 
-<script lang="ts" setup>
-import { h, ref } from 'vue'
-import { Home, Category, Find, Cart, My } from '@nutui/icons-vue-taro'
+<script setup lang="ts">
+import Taro from '@tarojs/taro';
+import { computed, reactive } from 'vue';
+import { useAppStore } from '@/store';
+import WeappSvg from '@/components/weapp-svg/index.vue';
 
-const active = ref(0);
-
-const List = ref([
+const appStore = useAppStore();
+const tabList = reactive([
   {
-    title: 'Home',
-    icon: h(Home)
+    id: 1,
+    path: '/pages/home/index',
+    icon: 'guide',
+    iconColor: '#2c2c2c',
+    iconColorSelected: '#00AA88',
+    iconSize: 50,
+    text: '首页',
   },
   {
-    title: 'Category',
-    icon: h(Category)
-  },
-  {
-    title: 'Find',
-    icon: h(Find)
-  },
-  {
-    title: 'Cart',
-    icon: h(Cart)
-  },
-  {
-    title: 'My',
-    icon: h(My)
+    id: 2,
+    path: '/pages/mine/index',
+    icon: 'account',
+    iconColor: '#2c2c2c',
+    iconColorSelected: '#00AA88',
+    text: '我的',
+    iconSize: 50,
   }
 ])
 
-const tabSwitch = (item: Record<string, unknown>, index: number) => {
-  console.log(item, index)
+const selected = computed(() => appStore.selectedTabIndex);
+
+const handleSwitch = (e: any) => {
+  appStore.setTabIndex(e.currentTarget.dataset.idx)
+  Taro.switchTab({
+    url: e.currentTarget.dataset.path
+  });
 }
+
 </script>
-
 <style lang="scss">
-
+.tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  .tabbar-list {
+    margin: 0 32px;
+    border-radius: 999px;
+    height: 100px;
+    background-color: #fff;
+    box-shadow: 0 6px 30px 5px rgba(0, 0, 0, 0.05);
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    .tabbar-item {
+      flex: 1;
+      text-align: center;
+      &__inner {
+        display: inline-flex;
+        flex-direction: column;
+        padding: 0 50px;
+        border-radius: 999px;
+        &.active {
+          background-color: #edf6ff;
+        }
+      }
+      .tabbar-text {
+        font-size: 28px;
+        color: #2c2c2c;
+        &.active {
+          color: #00AA88;
+        }
+      }
+    }
+  }
+  .zhanwei {
+    height: env(safe-area-inset-bottom);
+    background: transparent;
+  }
+}
 </style>
