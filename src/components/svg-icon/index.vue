fbee303c5e7a5e1b1a16a6c7acad728253cdc1e7
Damiyinliao <2911493102@qq.com>
2024-08-08 09:41:26 +0800

    feat: svg-icon component

--- src/components/svg-icon/index.vue
+++ src/components/svg-icon/index.vue
@@ -1,48 +1,50 @@
 <template>
-  <image
-    v-bind="attrs"
-    class="taro-image-component"
-    :src="icon[props.name]"
-    :style="mainStyle"
-  />
+  <svg v-if="!isWeapp" class="svg-icon-scoped" aria-hidden="true" :style="customStyle">
+    <use :href="iconName" />
+  </svg>
+  <Suspense v-else>
+    <template #default>
+      <weapp-svg :color="props.color" :name="props.name" :size="props.size" :radius="props.radius"></weapp-svg>
+    </template>
+    <template #fallback> <view></view> </template>
+  </Suspense>
 </template>
 
-<script lang="ts" setup>
-import { computed } from 'vue'
-import { useAttrs } from 'vue'
+<script setup lang="ts">
+import Taro from '@tarojs/taro';
+import { computed } from 'vue';
+import WeappSvg from '../weapp-svg/index.vue';
 
 interface Props {
-  name: string,
-  size?: number,
-  radius?: number|string
-
-}
-
-const attrs = useAttrs()
-
-const icon = {
-  'home': require('@/assets/icons/home.svg'),
-  'wechat': require('@/assets/svg/wechat.svg'),
-  'jd': require('@/assets/svg/jd.svg'),
-  'component': require('@/assets/svg/component.svg'),
-  'arrow-right': require('@/assets/svg/arrow-right.svg')
+  name: string;
+  color?: string;
+  size?: number;
+  radius?: number|string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  size: 30,
-  radius: 0
+  color: '',
+  size: 30
 });
+const isWeapp = process.env.TARO_ENV === 'weapp';
 
-const mainStyle = computed(() => ({
-  height: props.size + 'rpx',
-  width: props.size + 'rpx',
-  '--taro-image-radius': typeof props.radius === 'string' ? props.radius : props.radius + 'px'
-}));
+const iconName = computed(() => `#icon-${props.name}`);
+
+const customStyle = computed(() => ({
+  color: props.color,
+  width: Taro.pxTransform(props.size),
+  height: Taro.pxTransform(props.size),
+}))
+
+// if (!isWeapp) {
+//   import(`@/assets/icons/${props.name}.svg`);
+// }
 </script>
 
 <style lang="scss">
-.taro-image-component {
-  display: block;
-  border-radius: var(--taro-image-radius);
+.svg-icon-scoped {
+  flex-shrink: 0;
+  position: relative;
+  vertical-align: -2px;
 }
 </style>
